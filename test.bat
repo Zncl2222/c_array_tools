cfa0c3fda7a7637a9e9806b8caa517b1a36e10d5
Zncl2222 <3002shinning@gmail.com>
2023-07-06 00:32:39 +0800

    ci(cmake): add windows os build and test ci

--- test.bat
+++ test.bat
@@ -3,4 +3,11 @@ cd build
 cmake -G "MinGW Makefiles" ..
 make
 make test
+.\test\unittest_double.exe
+.\test\unittest_float.exe
+.\test\unittest_int.exe
+.\test\unittest_long.exe
+.\test\unittest_mt.exe
+.\test\unittest_short.exe
+cd ..
 @REM make memcheck // this is not avaliable in windows
